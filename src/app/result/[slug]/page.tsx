0cd64fb8ed133e0b0d994c8c7c9475d46ff039a9
Kyle Moy <kyle.l.moy@gmail.com>
2025-11-03 00:14:50 -0600

    Add page metadata and OG image

--- src/app/result/[slug]/page.tsx
+++ src/app/result/[slug]/page.tsx
@@ -6,6 +6,32 @@ import {
   RevealFx,
   Button,
 } from "@once-ui-system/core";
+import type { Metadata } from "next";
+import { getBallot } from "@/lib/dbAccess";
+
+// set dynamic metadata
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string | string[] }>;
+}): Promise<Metadata> {
+
+  const routeParams = await params;
+  const slugPath = Array.isArray(routeParams.slug)
+    ? routeParams.slug.join("/")
+    : routeParams.slug || "";
+  const ballot = await getBallot(slugPath);
+  if (!ballot) {
+    return {
+      title: "ranx-vote | ballot not found",
+      description: "The requested ballot could not be found.",
+    };
+  }
+  return {
+    title: `ranx-vote | results | ${ballot.name}`,
+    description: ballot.description,
+  };
+}
 
 export default async function Result({
   params,
--- src/app/vote/[slug]/page.tsx
+++ src/app/vote/[slug]/page.tsx
@@ -6,6 +6,32 @@ import {
   Button,
 } from "@once-ui-system/core";
 import ResponsePage from "../../../components/ResponsePage";
+import type { Metadata } from "next";
+import { getBallot } from "@/lib/dbAccess";
+
+// set dynamic metadata
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string | string[] }>;
+}): Promise<Metadata> {
+
+  const routeParams = await params;
+  const slugPath = Array.isArray(routeParams.slug)
+    ? routeParams.slug.join("/")
+    : routeParams.slug || "";
+  const ballot = await getBallot(slugPath);
+  if (!ballot) {
+    return {
+      title: "ranx-vote | ballot not found",
+      description: "The requested ballot could not be found.",
+    };
+  }
+  return {
+    title: `ranx-vote | vote | ${ballot.name}`,
+    description: ballot.description,
+  };
+}
 
 export default async function Vote({
   params,
